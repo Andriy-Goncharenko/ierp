60c5e2960db259d3796a3eecc1bddffeeee83413
dmitrylineup <kotelevsky.dima@gmail.com>
2018-08-27 21:11:51 +0300

    set static

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 
-app.use('/static', express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
